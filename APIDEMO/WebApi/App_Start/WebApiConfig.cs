be1093b0a46e16a57530a878050172c343eb9593
humanawinners <humanawinners@gmail.com>
2020-02-02 00:03:40 +0530

    Adding custom formatter class so that the accept header in the request is a;so reflected with applicatin /json compared to the last

--- APIDEMO/WebApi/App_Start/WebApiConfig.cs
+++ APIDEMO/WebApi/App_Start/WebApiConfig.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
 namespace WebApi
 {
+    public class CustomJsonFormatter : JsonMediaTypeFormatter
+    {
+        public CustomJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/Json");
+
+        }
+
+    }
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -36,7 +55,12 @@ namespace WebApi
 
 
             //making changes so that by default when the request is raised from browser we make the api to return Json
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            // using the above we will still get the accept header as text/html but not tas application /json
+
+            //this approach of Adding own custom so we can reflect in the header type accept
+            config.Formatters.Add(new CustomJsonFormatter());
+
         }
     }
 }
